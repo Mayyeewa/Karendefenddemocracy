ece3e0d7790588d3b4f5236feec688b9a18161d7
Penprapa Calhoun <mayyeewa@yahoo.com>
2021-04-26 17:30:11 -0400

    first commit

--- src/week3/DeleteCharactersFromString.java
+++ src/week3/DeleteCharactersFromString.java
@@ -1,11 +1,21 @@
 package week3;
 
+import java.util.Scanner;
+
 import acm.program.ConsoleProgram;
 
 public class DeleteCharactersFromString extends ConsoleProgram {
-	
-	public void run() {
+
+	public static void main(String[] args) {
+		String str = "This is a test";
+		String str1 = "Summer is here";
+		String str2 = "---O--";
+		
+// will be back to work on you later 
+		System.out.println(str.replace("t", ""));
+		System.out.println(str1.replace("e", ""));
+		System.out.println(str2.replace("-", ""));
+		
 
 	}
-	
 }
